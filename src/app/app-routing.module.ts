ac6a234d593edd87ce1f32ab4659d95bc45fa8bd
asmithasatesh <asmithasatesh@gmail.com>
2021-10-11 13:13:14 +0530

    [Asmitha] Added: Header for Quanment

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MeasurementComponent } from './Components/measurement/measurement.component';
 
-const routes: Routes = [];
+const routes: Routes = [  {path:'quanment', component:MeasurementComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
